b6feb2d6b507ac6668906b1327c361c07227f382
Igor Manukyan <igormanukyan@mail.com>
2024-04-17 12:12:04 +0300

    Создали репозиторий с семинаром

---
+++ Seminars/Sem_1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Console.WriteLine("Input number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(a * a);
+
+// // Напишите программу, которая 
+// на вход принимает 
+// два целых числа и проверяет, 
+// является ли первое число квадратом 
+// второго.
+
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 == num2 * num2)
+// {
+//     System.Console.WriteLine($"{num1} is quad of {num2}");
+// }
+// else
+// {
+//     System.Console.WriteLine($"{num1} is not quad of {num2}");
+// }
+
+// Напишите программу, которая на вход принимает 
+// целое число N, а на выходе показывает все 
+// целые числа в промежутке от -N до N.
+
+// Console.Write("Input N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int start = -n;
+// while(start <= n)
+// {
+//     System.Console.Write(start + " ");
+//     start ++;
+// }
+
+// Напишите программу, которая принимает на вход 
+// трёхзначное целое число и на выходе 
+// показывает сумму первой и последней 
+// цифры этого числа.
+
+// Console.Write("Input three-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num >= 100 && num <= 999)
+// {
+//     int ed = num % 10;
+//     int sot = num / 100;
+//     System.Console.WriteLine($"Sum of {ed} and {sot} -> {ed + sot}");
+// }
+// else
+// System.Console.WriteLine("You input not three-digit number");
---
+++ Seminars/Sem_2/Program.cs
@@ -0,0 +1,81 @@
+﻿
+// void NumToQuad (int num)
+// {
+//     System.Console.WriteLine(num * num);
+// }
+
+// int NumToQuadInt (int num)
+// {
+//     return num * num;
+// }
+
+// System.Console.WriteLine("Input number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(NumToQuadInt(a));
+
+
+// Напишите программу, которая принимает на вход 
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+// int DeleteSecondDigit(int num)
+// {
+//     if (num >= 100 && num <= 999)
+//     {
+//         int ed = num % 10;
+//         int sot = num / 100;
+//         return sot * 10 + ed;
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("You input not three-digit number");
+//         return 0;
+//     }
+// }
+
+// int SecondToThirPower(int num)
+// {
+//     if (num >= 100 && num <= 999)
+//     {
+//         int ed = num % 10;
+//         int dec = num / 10 % 10;
+//         int result = 1;
+//         for (int i = 0; i < ed; i++)
+//         {
+//             result = result * dec;
+//         }
+//         return result;
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("You input not three-digit number");
+//         return 0;
+//     }
+// }
+
+// Console.Write("Input three-digit number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(SecondToThirPower(num));
+
+// Напишите программу, которая будет принимать на вход два числа 
+// и выводить, является ли первое число кратным второму. 
+// Если первое число некратно второму, то программа выводит остаток от деления.
+
+
+void CheckIfDevides (int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        System.Console.WriteLine($"{num1}, {num2} -> yes");
+    }
+    else
+    {
+        System.Console.WriteLine($"{num1}, {num2} -> no, {num1 % num2}");
+    }
+}
+
+Console.Write ("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+CheckIfDevides(num1, num2);
